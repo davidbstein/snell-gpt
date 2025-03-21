33f7cd0f0272ec14035f8c5e8991b6b2b4220e92
Stein <davidbstein@gmail.com>
2025-03-21 11:29:59 -0400

    Update tree_viewer.js

--- docs/static/tree_viewer.js
+++ docs/static/tree_viewer.js
@@ -17,7 +17,7 @@ window._progressValue = 0;
 
 function setProgressBar(msg) {
   window._progressValue = 0;
-  window.querySelector("#progress-bar-holder").innerHTML = `<div style='width:50vw;margin: calc(10vh) 25vw;'>${msg}<div id='progress-bar'><div id='progress'></div><div id='progress-info'></div></div></div>`;
+  document.getElementById("progress-bar-holder").innerHTML = `<div style='width:50vw;margin: calc(10vh) 25vw;'>${msg}<div id='progress-bar'><div id='progress'></div><div id='progress-info'></div></div></div>`;
 }
 
 function updateProgress(progress) {
